53a09db5ec6c3831b4e97135d1f7f4daad993ec5
Nitin <tulswani19@gmail.com>
2017-08-08 11:52:56 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+<<<<<<< HEAD
 
 class App extends Component {
   static displayName = "App";
+=======
+import Demo from './components/Demo';
+
+class App extends Component {
+  static displayName = 'App';
+>>>>>>> d45443141f1c516d15b4b1fabecb197b84a64b40
 
   render() {
     return (
       <div>
         <Header />
+<<<<<<< HEAD
+=======
+        <Demo />
+>>>>>>> d45443141f1c516d15b4b1fabecb197b84a64b40
       </div>
     );
   }
